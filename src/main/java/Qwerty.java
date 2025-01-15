32b98db7c7cdbb47fc47f5a4db4aac1a180360dc
weien02 <weien02@gmail.com>
2025-01-16 02:30:47 +0800

    Level 2. Add, List

--- src/main/java/Qwerty.java
+++ src/main/java/Qwerty.java
@@ -1,6 +1,22 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Qwerty {
+
+    public static ArrayList<String> list = new ArrayList<>();
+
+    public static void printList() {
+        for (int i = 0; i < list.size(); i++) {
+            int index = i + 1;
+            System.out.println("" + index + ". " + list.get(i));
+        }
+    }
+
+    public static void addToList(String item) {
+        list.add(item);
+        System.out.println("added: " + item);
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -12,7 +28,11 @@ public class Qwerty {
             if (userInput.equals("bye")) {
                 break;
             }
-            System.out.println(userInput);
+            if (userInput.equals("list")) {
+                printList();
+            } else {
+                addToList(userInput);
+            }
         }
         System.out.println("See ya!");
         scanner.close();
